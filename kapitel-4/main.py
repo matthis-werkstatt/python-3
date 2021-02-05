9bd36e12b46641359144ee9c2730e39558faa5ce
Matthis Kamin <matthiskamin@gmail.com>
2021-02-05 21:52:27 +0100

    Das programm funktioniert

---
+++ kapitel-4/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+geheimnis = 1337
+versuch = -1
+zaehler =0
+
+while versuch != geheimnis:
+
+    versuch = int(input("Raten Sie: "))
+
+    if versuch < geheimnis:
+        print ("Zu klein")
+
+    if versuch > geheimnis:
+        print("Zu groß")
+
+    zaehler = zaehler + 1
+
+print("Super, sie haben in ", zaehler, "Versuchen geschafft")
